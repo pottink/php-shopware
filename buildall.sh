82e36e86ee6c4cc68d5d618d3380a81332e31d9a
Jelle Deneweth <jelle.deneweth@gmail.com>
2023-06-01 07:46:36 +0200

    Create 8.2 version
    Signed-off-by: Jelle Deneweth <jelle.deneweth@gmail.com>

--- buildall.sh
+++ buildall.sh
@@ -1,3 +1,6 @@
 cd 8.1
 docker build --no-cache -t pottink/php-shopware:8.1 .
 docker push pottink/php-shopware:8.1
+cd ../8.2
+docker build --no-cache -t pottink/php-shopware:8.2 .
+docker push pottink/php-shopware:8.2
