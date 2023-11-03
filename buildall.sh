b7ddb924167d6b6c5293cdf18515a9f5c019d020
Jelle Deneweth <jelle.deneweth@gmail.com>
2023-11-03 11:53:55 +0100

    Update to use buildx and multiple platforms
    Signed-off-by: Jelle Deneweth <jelle.deneweth@gmail.com>

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,4 @@
 cd 8.1
-docker build --no-cache -t pottink/php-shopware:8.1 .
-docker push pottink/php-shopware:8.1
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --no-cache -t pottink/php-shopware:8.1 --push .
 cd ../8.2
-docker build --no-cache -t pottink/php-shopware:8.2 .
-docker push pottink/php-shopware:8.2
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --no-cache -t pottink/php-shopware:8.2 --push .
