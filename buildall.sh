381dbbc91e389d8c906d0170ee443f8017bb6f16
Jelle Deneweth <jelle.deneweth@gmail.com>
2024-06-14 12:55:17 +0200

    Add PHP8.3
    Signed-off-by: Jelle Deneweth <jelle.deneweth@gmail.com>

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,6 @@
 cd 8.1
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --no-cache -t pottink/php-shopware:8.1 --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t pottink/php-shopware:8.1 --push .
 cd ../8.2
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --no-cache -t pottink/php-shopware:8.2 --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t pottink/php-shopware:8.2 --push .
+cd ../8.3
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t pottink/php-shopware:8.3 --push .
